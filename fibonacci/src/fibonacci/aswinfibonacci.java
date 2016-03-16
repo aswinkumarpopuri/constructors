82d540e4560b3f9a69d4ef09f1904bc04d5e33d1
aswinkumarpopuri <popuriaswinkumar@gmail.com>
2016-03-16 13:58:51 -0500

    Added files via upload

---
+++ fibonacci/src/fibonacci/aswinfibonacci.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package fibonacci;
+
+/**
+ * @author aswinikumar
+ *
+ */
+public class aswinfibonacci {
+	public static void main(String[] args){
+		System.out.println("enter the number");
+	}
+ }
+public
+		int p=0;
+		int q=1;
+		
+	System.out.println(p);
+	System.out.println(q);
+	int n;
+	n=p+q;
+	System.out.print(+n);
+
+	}
+
+}
